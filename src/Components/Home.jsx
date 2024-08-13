2447204348ade9a306b5989697d057ce315e2784
Ashok0077 <vaishnavashok7297@gmail.com>
2024-08-13 14:08:15 +0530

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ import logo from '../assets/logo.jpg'
 const Home = () =>{
    return(
     <>
-    <img style={{maxWidth: "700px"}} src={logo} alt="Youtube Logo"/>
+    <img style={{maxWidth: "700px", width : "100%"}} src={logo} alt="Youtube Logo"/>
     <form action="https://www.youtube.com/results">
         <input type="text" name="search_query" required />
         <button type="submit">Search</button>
